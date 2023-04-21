a95ae3427531a6236c390656d606ecfa0ca49d94
pris-ofmens <prisofmens@outlook.com>
2023-04-20 23:01:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints by a new line, to the standard error.
- * Return: (1) if return value is 1 it means sucessful.
+ * main - A C program that prints by a new line, to the standard error
+ * Return: (1) if return value is 1 it means sucessful
  */
 int main(void)
 {
-	puts,
-		("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
