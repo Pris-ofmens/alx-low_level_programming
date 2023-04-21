83c78a6bd9fdaa6378901fbfbe247df8f913ed9d
pris-ofmens <prisofmens@outlook.com>
2023-04-20 22:12:58 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C program that prints by a new line, to the standard error.
+ * Return: (1) if return value is 1 it means sucessful.
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
