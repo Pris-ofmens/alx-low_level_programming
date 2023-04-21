5d0fc5741fb38286b989e138f4fe39bacf4749d7
pris-ofmens <prisofmens@outlook.com>
2023-04-20 20:44:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints using the function puts.
+ * Return: 0 (means return value successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
