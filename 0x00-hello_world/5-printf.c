5078f3fbabb38de69becba51ca57f2f9125f9a2c
pris-ofmens <prisofmens@outlook.com>
2023-04-20 21:04:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints with the function printf
+ * Return: (0) if the return is 0 then it means successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
