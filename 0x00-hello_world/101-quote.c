cb973094f926f5ed2564b54d27ce86279ff70f1d
pris-ofmens <prisofmens@outlook.com>
2023-04-22 08:55:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A C Standard error program
+ * main - entry
+ * description - A C Standard error program
  * Return: (1) means successful
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2);
+	("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
