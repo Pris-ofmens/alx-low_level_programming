8ca6aa46ed8d69ed54c2384aa85549bd23a53ec5
pris-ofmens <prisofmens@outlook.com>
2023-04-21 20:00:50 -0700

    0-positive_or_negative.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
- * main - A C program that prints a new line, to the standard error
- * Return (1)  means sucessful
+/**
+ * main - A C Standard error program
+ * Return: (1) means successful
  */
 int main(void)
 {
-put("and that piece of art is useful.\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+my firth README.md
