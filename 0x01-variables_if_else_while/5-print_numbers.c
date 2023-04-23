32629a486dbde8a8b3c4b0f68a799595abc51188
pris-ofmens <prisofmens@outlook.com>
2023-04-23 08:07:06 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,17 +2,20 @@
 
 /**
  * main - start
- * Description: single digit numbers of base 10
+ * Description: single digit numbers of base ten
  * Return: (0) always
  */
+
 int main(void)
 {
+
 	int n;
 
-	for (n == 0; n <= 9; n++);
-	{
-		printf("%d", n);
-	}
+	for (n = 0; n <= 9; n++)
+
+	printf("%d", n);
+
 	printf("\n");
+
 	return (0);
 }
