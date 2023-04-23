7bcfc5908b4b73349bd19b82a6ac4cc0cec16b72
pris-ofmens <prisofmens@outlook.com>
2023-04-23 09:27:52 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Description: combination of single digit numbers
+ * Return: (0) always
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 48; r <= 57; r++)
+	{
+		putchar(r);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
