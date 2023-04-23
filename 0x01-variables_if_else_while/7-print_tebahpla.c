d7803af7cb4116eb6a9b26557443bcd5f0b9f702
pris-ofmens <prisofmens@outlook.com>
2023-04-23 08:51:26 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
- * main - start
- * Description:Lower case alphabet in reverse
- * Return: (0) always
+ *main - start
+ *Description:Lower case alphabet in reverse
+ *Return: (0) always
  */
 int main(void)
 {
 	int n = 122;
 
 	while (n >= 97)
-
+	{
 		putchar(n);
-	n--;
+		n--;
+	}
 
 	putchar('\n');
 	return (0);
