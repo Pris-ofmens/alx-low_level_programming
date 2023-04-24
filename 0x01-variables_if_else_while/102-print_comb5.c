0b7e68ff34eb07d73f86a4899e374e544697f5ea
pris-ofmens <prisofmens@outlook.com>
2023-04-24 06:37:15 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - start
+ * Description: combinations of two two-digit numbers
+ * Return: (0) always
+ */
+int main(void)
+{
+	int a, s, d;
+
+	for (a = '0'; a < '9'; a++)
+        {
+                for (s = a + 1; s <= '9'; s++)
+                {
+                        for (d = s + 1; d <= '9'; d++)
+                        {
+                                if ((s != a)!= d)
+                                {
+                                        putchar(a);
+                                        putchar(s);
+                                        putchar(d);
+
+                                        if (a == '7' && s == '8')
+                                                continue;
+
+                                        putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	
+	return (0);
+}
