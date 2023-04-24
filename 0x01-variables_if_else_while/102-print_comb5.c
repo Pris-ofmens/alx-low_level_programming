00c70588b4ab52d41c04caae5c2744e23af37fb2
pris-ofmens <prisofmens@outlook.com>
2023-04-24 07:17:31 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,30 +9,24 @@
  */
 int main(void)
 {
-	int a, s, d;
+	int b, h;
 
-	for (a = '0'; a < '9'; a++)
-        {
-                for (s = a + 1; s <= '9'; s++)
-                {
-                        for (d = s + 1; d <= '9'; d++)
-                        {
-                                if ((s != a)!= d)
-                                {
-                                        putchar(a);
-                                        putchar(s);
-                                        putchar(d);
-
-                                        if (a == '7' && s == '8')
-                                                continue;
-
-                                        putchar(',');
-					putchar(' ');
-				}
-			}
+	for (b = 0; b <= 98; b++)
+	{
+	for (h = b + 1; h <= 99; h++)
+	{
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+		putchar((h / 10) + '0');
+		putchar((h % 10) + '0');
+		{
+			if (b == 98 && h == 99)
+				continue;
+		putchar(',');
+		putchar(' ');
 		}
 	}
+	}
 	putchar('\n');
-	
 	return (0);
 }
