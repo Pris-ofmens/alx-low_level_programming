e7639fa649e17c169d4e946aa6c0d77716ee729b
pris-ofmens <prisofmens@outlook.com>
2023-04-26 09:55:02 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,21 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdio.h>
 
 /**
- * main -  start
- * Description:function that prints alphabet in lowercase
+ * main - start
+ * Description:function that prints alphabets in lower case
  * Return: (0) success
  */
 
-void print_alphabet(void)
+int void print_alphabet(void)
 {
-        char g = 'a';
-
-        while (g <= 'z')
-        {
-                _putchar(g);
-                g++;
-        }
-        _putchar('\n');
+	char ('g' = 'a')
+		while
+			('g' <= 'z');
+	{
+		_putchar('g');
+		g++;
+	}
+	_putchar('\n');
 }
-
-
