a5552988abc653d0ad02a4fd7ccc42a381cb7777
pris-ofmens <prisofmens@outlook.com>
2023-04-26 14:20:56 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
-
- * _islower - Checks for lowercase letters
-
- * @c: Character to be checked
-
+ * _islower - Checks for lowercase alphabet
+ * @c: Character  checks
  * Return: 1 if character is lowercase or 0 if otherwise
-
  */
 
 int _islower(int c)
-
 {
 	if (c >= 'a' && c <= 'z')
-
-                return (1);
-        else
-                return (0);
-
+		return (1);
+	else
+		return (0);
 }
