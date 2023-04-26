aa7fc9394d7ab412b36b71942b8bee62e597766d
pris-ofmens <prisofmens@outlook.com>
2023-04-25 22:39:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+#include <unistd.h>
+
+
+/**
+
+ * print_alphabet - start
+
+ * Return:0
+
+ */
+
+void print_alphabet(void)
+
+{
+
+        char c = 'a';
+
+        while (c <= 'z')
+
+        {
+                _putchar(c);
+
+                c++;
+        }
+        _putchar('\n');
+}
+
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - start
+ * Description: print alphabet ten times lowercase
+ * Return: (0)
+ */
+
+int void print_alphabet_x10(void);
+{
+	char num, letter;
+	{
+		for (num = 0; num <= 9; num++)
+		{
+			for (letter = 'a'; letter <= 'z'; letter++)
+		}
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0);
+}
