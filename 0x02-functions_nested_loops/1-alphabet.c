e6f566d684b4419ba508854e6474ff98ab0c3189
pris-ofmens <prisofmens@outlook.com>
2023-04-26 10:43:51 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,20 @@
 #include "main.h"
 #include <unistd.h>
-#include <stdio.h>
 
 /**
- * main - start
+ * main - void print_alphabet
  * Description:function that prints alphabets in lower case
  * Return: (0) success
  */
 
-int void print_alphabet(void)
+int main(void)
 {
-	char ('g' = 'a')
+	char g = 'a';
 		while
-			('g' <= 'z');
-	{
-		_putchar('g');
-		g++;
-	}
-	_putchar('\n');
+			(g <= 'z');
+		{
+			_putchar(g);
+			g++;
+		}
+		_putchar('\n');
 }
