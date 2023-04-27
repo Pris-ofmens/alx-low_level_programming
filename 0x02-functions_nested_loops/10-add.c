4f089fccd8ea2c10d259d480431034371469468f
pris-ofmens <prisofmens@outlook.com>
2023-04-26 18:21:22 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and gives the result
+ * @q: number added
+ * @w: number added
+ * Return: Always 0
+ */
+int add(int q, int w)
+{
+	return (q + w);
+}
