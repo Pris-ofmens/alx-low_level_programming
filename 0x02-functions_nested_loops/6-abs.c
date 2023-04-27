3914ef368e16013d3e0e09a2a8568ae5f36df01a
pris-ofmens <prisofmens@outlook.com>
2023-04-26 17:11:43 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an intega
+ * @n: the int to be checked
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
